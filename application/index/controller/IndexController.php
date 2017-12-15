e20345dfe15ce39ef29d82ba7c2eabccd498001e
zhouweiyong <16681805@qq.com>
2017-12-15 18:04:04 +0800

    ok

--- application/index/controller/IndexController.php
+++ application/index/controller/IndexController.php
@@ -19,6 +19,7 @@ class IndexController extends BaseController
         return $this->fetch("index");
     }
 
+    //显示全部
     public function detail()
     {
         $userName = input("get.userName");
@@ -32,5 +33,51 @@ class IndexController extends BaseController
         $this->assign("data", $detail);
 
         return $this->fetch();
+    }
+
+    //显示有数据的部分
+    public function detail2()
+    {
+        $userName = input("get.userName");
+        $gtime = input("get.gtime");
+        $num = Db::name("gz_num")->where("gtime", $gtime)->value("num");
+        $header = Db::name("gz_header")->where("gtime", $gtime)->find();
+        $detail = Db::name("gz_data")->where("col0", $userName)->where("gtime", $gtime)->find();
+
+        //查找空数据的项
+        $epty = array();
+        while (list($key, $val) = each($detail)) {
+            if ($val == null || empty($val)) {
+                array_push($epty, $key);
+            }
+        }
+
+        //删除空数据项
+        $len = sizeof($epty);
+        for ($i = 0; $i < $len; $i++) {
+            $key = array_pop($epty);
+            unset($header[$key]);
+            unset($detail[$key]);
+        }
+
+
+        $this->assign("gtime", $gtime);
+        $this->assign("num", $num);
+        $this->assign("header", $header);
+        $this->assign("data", $detail);
+        return $this->fetch("detail");
+    }
+
+    public function show()
+    {
+        $arr = array();
+        array_push($arr, "hell");
+        array_push($arr, "hell2");
+        $len = sizeof($arr);
+        for ($i = 0; $i < $len; $i++) {
+            dump($arr[$i]);
+        }
+        dump($arr);
+        return "IIIIIIIIII";
     }
 }
